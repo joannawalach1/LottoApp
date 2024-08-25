a0e37e6623d915e5428483c085b0ee8679544d45
joannawalach1 <joanna.walach@op.pl>
2024-08-26 01:16:24 +0200

    add NumberReceiverFacade and tests

--- src/main/java/com/javaready/LottoApp/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/com/javaready/LottoApp/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,4 +1,26 @@
 package com.javaready.LottoApp.domain.numberreceiver;
 
+import java.util.List;
+import java.util.Set;
+
 public class NumberReceiverFacade {
+
+    public String inputNumbers(Set<Integer> numbers) {
+        List<Integer> filteredNumbers = filteredNumbers(numbers);
+        if (areAllNumbersInRange(numbers)) {
+            return "success";
+        }
+        return "fail";
+    }
+
+    private static boolean areAllNumbersInRange(Set<Integer> numbers) {
+        return numbers.size() == 6;
+    }
+
+    private List<Integer> filteredNumbers(Set<Integer> numbers) {
+        return numbers.stream()
+               .filter(number -> number >= 1)
+               .filter(number -> number <= 99)
+               .toList();
+    }
 }
--- src/test/java/com/javaready/LottoApp/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/com/javaready/LottoApp/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,17 +1,51 @@
 package com.javaready.LottoApp.domain.numberreceiver;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class NumberReceiverFacadeTest {
+import java.util.Set;
 
-    @BeforeEach
-    void setUp() {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberReceiverFacadeTest {
+
+
+    @Test
+    void should_return_success_when_user_gave_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String numbers = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        //then
+        assertThat(numbers).isEqualTo("success");
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void should_return_failed_when_user_gave_less_than_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String numbers = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5));
+        //then
+        assertThat(numbers).isEqualTo("fail");
+    }
+
+    @Test
+    void should_return_failed_when_user_gave_more_than_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String numbers = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6, 7));
+        //then
+        assertThat(numbers).isEqualTo("fail");
+    }
+
+    @Test
+    void should_return_failed_when_user_gave_out_of_range_number() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String numbers = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3000, 4, 5, 6, 7));
+        //then
+        assertThat(numbers).isEqualTo("fail");
     }
 }
